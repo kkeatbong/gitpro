ede92f1a099b1f80f76c19b7219d042906770e7c
kkeatbong <1925084819@qq.com>
2019-12-11 15:36:42 +0800

    aaa

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.zking.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: w541
+ * Date: 2019/12/11
+ * Time: 15:28
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("=============");
+        System.out.println("=============");
+    }
+}
---
+++ a.java
@@ -0,0 +1,10 @@
+package com.zking.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: w541
+ * Date: 2019/12/11
+ * Time: 15:35
+ */
+public class a {
+}
